cf24ea31ec9caae436ad2820d516f05429e64227
Alejandro <janderl@gmail.com>
2022-03-02 23:21:42 +0100

    Add tests

--- spec/hanikamu/service_spec.rb
+++ spec/hanikamu/service_spec.rb
@@ -16,10 +16,9 @@ RSpec.describe Hanikamu::Service do
     end
   end
 
-  describe "#call" do
+  describe ".call" do
     context "with a failing service" do
       let(:failing_service) do
-        stub_const("TestFooModule", Module.new)
         class TestFooModule::Bar < Hanikamu::Service
           CustomError = Class.new(self::Error)
           def call!
@@ -36,9 +35,33 @@ RSpec.describe Hanikamu::Service do
         expect(failing_service.call.failure.class.superclass).to be(Hanikamu::Service::Error)
       end
 
+      context "when passing the wrong argument type" do
+        let(:failing_service) do
+          class TestFooModule::Bar < Hanikamu::Service
+            attribute :some_string, Dry::Types["string"]
+
+            def call!
+              "hola"
+            end
+          end
+          TestFooModule::Bar
+        end
+
+        describe ".call" do
+          it "returns a monadic Failure response" do
+            expect(failing_service.call(some_string: 3)).to be_a(Dry::Monads::Failure)
+          end
+        end
+
+        describe ".call!" do
+          it "returns a Dry::Struct::Error" do
+            expect { failing_service.call!(some_string: 3) }.to raise_error(Dry::Struct::Error)
+          end
+        end
+      end
+
       context "when using the custom service error" do
         let(:failing_service) do
-          stub_const("TestFooModule", Module.new)
           class TestFooModule::Bar < Hanikamu::Service
             def call!
               raise Error, "Oh, no!"
@@ -54,6 +77,29 @@ RSpec.describe Hanikamu::Service do
         end
       end
 
+      context "when the error is not manageable by Hanikamu::Service" do
+        let(:failing_service) do
+          class TestFooModule::Bar < Hanikamu::Service
+            def call!
+              raise StandardError, "Unexpected error"
+            end
+          end
+          TestFooModule::Bar
+        end
+
+        describe ".call" do
+          it "raises the error" do
+            expect { failing_service.call }.to raise_error(StandardError)
+          end
+        end
+
+        describe ".call!" do
+          it "raises the error" do
+            expect { failing_service.call! }.to raise_error(StandardError)
+          end
+        end
+      end
+
       it "raises an error when is a none WhiteListedError" do
         allow(described_class).to receive(:call).and_raise(StandardError.new("Oh, no!"))
 
@@ -64,18 +110,30 @@ RSpec.describe Hanikamu::Service do
 
   context "when using response helper" do
     let(:response_service) do
-      stub_const("TestFooModule", Module.new)
       class TestFooModule::Bar < Hanikamu::Service
+        attribute :hello, Dry::Types["string"]
         def call!
-          response hola: "caracola"
+          response hola: hello
         end
       end
       TestFooModule::Bar
     end
 
-    it "responses with a Struct class" do
-      expect(response_service.call!).to be_a(Struct)
-      expect(response_service.call!.hola).to be("caracola")
+    let(:conch) { "caracola" }
+
+    describe ".call!" do
+      it "responses with a Struct class" do
+        expect(response_service.call!(hello: conch)).to be_a(Struct)
+        expect(response_service.call!(hello: conch).hola).to be(conch)
+      end
     end
+
+    describe ".call" do
+      it "responses with a Struct class" do
+        expect(response_service.call(hello: conch)).to be_a(Dry::Monads::Success)
+        expect(response_service.call(hello: conch).success.hola).to be(conch)
+      end
+    end
+
   end
 end
